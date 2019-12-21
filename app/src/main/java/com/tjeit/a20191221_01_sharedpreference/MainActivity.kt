b2e9b72f47982bdd839512de3c8575c6414bdb99
gyucode <gyucode@gmail.com>
2019-12-21 11:58:02 +0900

    ID저장 체크여부를 저장 / 반영

--- app/src/main/java/com/tjeit/a20191221_01_sharedpreference/MainActivity.kt
+++ app/src/main/java/com/tjeit/a20191221_01_sharedpreference/MainActivity.kt
@@ -2,6 +2,9 @@ package com.tjeit.a20191221_01_sharedpreference
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.tjeit.a20191221_01_sharedpreference.utils.ContextUtil
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -14,9 +17,38 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+//        Q1. 아이디 저장 로직 구현
+//        loginBtn이 눌리면
+//        idCheckBox가 체크되어있는지 검사 (구글링)
+//        되어 있다면 idEdt에 적혀있는 값을 userId 라는 val로 저장
+
+        loginBtn.setOnClickListener {
+            if (idCheckBox.isChecked) {
+                val userId = idEdt.text.toString()
+
+                ContextUtil.setUserId(mContext, userId)
+            }
+        }
+//        Q2. 아이디 저장 체크박스 체크여부 (Boolean)저장
+//        IdCheckBox가 체크되면(구글링)
+//        체크된 값(체크/해제)을 contextUtil - SharePreference활용해서 저장
+//        앱이 켜질때 set
+
+        idCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+            Log.d("isChecked값", "${isChecked}")
+            ContextUtil.setUserIdSave(mContext, isChecked)
+
+        }
+
+
+
+
     }
 
     override fun setValues() {
+        idEdt.setText(ContextUtil.getUserId(mContext))
+
+        idCheckBox.isChecked = ContextUtil.getUserIdSave(mContext)
 
     }
 
--- app/src/main/java/com/tjeit/a20191221_01_sharedpreference/utils/ContextUtil.kt
+++ app/src/main/java/com/tjeit/a20191221_01_sharedpreference/utils/ContextUtil.kt
@@ -4,28 +4,44 @@ import android.content.Context
 
 class ContextUtil {
 
-//    우리가 사용할 Preference의 이름 : 메모장 - 저장/로드할 파일이름
-    val prefName = "MyProjectPreference"
+    companion object{
+        //    우리가 사용할 Preference의 이름 : 메모장 - 저장/로드할 파일이름
+        val prefName = "MyProjectPreference"
 
-//    사용자의 ID를 (저장-set/로드-get) 할때 사용하는 항목의 이름을 상수로 저장.
-    val USER_ID = "USER_ID"
+        //    사용자의 ID를 (저장-set/로드-get) 할때 사용하는 항목의 이름을 상수로 저장.
+        val USER_ID = "USER_ID"
+        val USER_ID_SAVE = "USER_ID_SAVE"
 
-//    userId를 저장하는 기능을 구현한 함수
-    fun setUserId(context: Context, inputId: String) {
+        //    userId를 저장하는 기능을 구현한 함수
+        fun setUserId(context: Context, inputId: String) {
 //        메모장에서 txt파일을 여는것과 같은 행위
 //        SharedPreferenece를 열어줌
-        val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
 
 //        열어준 preference에 USER_ID항목에 inpudId변수의 값을 저장 / 확정
-        pref.edit().putString(USER_ID, inputId).apply()
+            pref.edit().putString(USER_ID, inputId).apply()
+
+        }
+        fun setUserIdSave(context: Context,needSave:Boolean){
+            val pref = context.getSharedPreferences(prefName,Context.MODE_PRIVATE)
+            pref.edit().putBoolean(USER_ID_SAVE,needSave).apply()
+
+        }
+        fun getUserId(context: Context) : String {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+
+            return pref.getString(USER_ID, "")!!
+        }
+
+
+        fun getUserIdSave(context: Context):Boolean{
+            val pref = context.getSharedPreferences(prefName,Context.MODE_PRIVATE)
+            return pref.getBoolean(USER_ID_SAVE,false)
+        }
 
     }
 
-    fun getUserId(context: Context) : String {
-        val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
 
-        return pref.getString(USER_ID, "")!!
-    }
 
 
 }
